c39abe91f4987ffe45608780d43208bd3f5cab24
case-steamer <hipster1911@gmail.com>
2026-01-08 15:25:23 -0600

    added rudimentary breakdownDirectory function to the Brain class, and called it from the publish function.
    Function only prints to terminal right now. Also, apparently the '~' character is a no-go. Need to hardcode the address for the home directory into the program.

--- src/local/work/Brain.java
+++ src/local/work/Brain.java
@@ -4,13 +4,14 @@ import local.work.panels.BrainClient;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Stack;
 
 public class Brain {
 
-    private static FileSystem fileSystem;
     private static String rootDir;
     private static String currentLocation;
     private static Window window;
@@ -18,9 +19,26 @@ public class Brain {
     public static Stack<String> history;
     private static boolean caller;
 
+    public void breakdownDirectory(String path) {
+        Path filepath = Paths.get(path);
+        if (Files.exists(filepath) && Files.isDirectory(filepath)) {
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(filepath)) {
+                for (Path entry : stream) {
+                    System.out.println(entry.getFileName());
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Listing error");
+            }
+        }
+        else {
+            System.out.println("Invalid path");
+        }
+    }
+
     public void publish(String string) {
         setBackButtonState();
-        currentLocation = string;
+        breakdownDirectory(string);
         for (JPanel panel : panels) {
             if (panel instanceof BrainClient) {
                 ((BrainClient) panel).update(string);
@@ -55,7 +73,6 @@ public class Brain {
         this.window = window;
         this.panels = window.getPanels();
         history = new Stack<String>();
-        fileSystem = FileSystems.getDefault();
         rootDir = String.valueOf('/'); // Change this logic if extending this application to Windows or Mac.
         currentLocation = null;
         publish(rootDir);
